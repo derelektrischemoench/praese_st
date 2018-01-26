a9a6bd11c0ee71e0301b9bcb9c2e2ca9c1f7e138
Chris <baederchris@gmail.com>
2018-01-26 11:23:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# README #
+### How do I get set up? ###
+
+- cd to reveal.js...
+- run npm start
+- profit
---
+++ reveal.js-3.5.0/RUN.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "presentation started, browser launched. Have a nice day."
+
+npm start
+
